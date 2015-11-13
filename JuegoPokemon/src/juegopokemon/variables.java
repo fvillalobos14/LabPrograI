fc2c5612527e5d060b2ccd75a1fc6cea0e79867f
korrai <francisavp@hotmail.com>
2015-11-13 07:50:52 -0600

    juego pokemon

---
+++ JuegoPokemon/src/juegopokemon/variables.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegopokemon;
+
+/**
+ *
+ * @author Francis
+ */
+public class variables {
+    public static int  vida1, vida2, vida3, vida4;
+}
